d90305bfecbc92631082f2a347faace5c86ca57d
AndrewP3 <andrewpowell097@gmail.com>
2021-05-16 12:58:38 -0700

    Fourth commit

--- questions.js
+++ questions.js
@@ -13,13 +13,13 @@ let questions = [
     },
     {
         numb: 2,
-        question: " ______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
-        answer: "<SCRIPT>",
+        question: "Scripting language are",
+        answer: "High Level Programming language",
         options: [
-            "<SCRIPT>",
-            "<BODY>",
-            "<HEAD>",
-            "<TITLE>"
+            "High Level Programming language",
+            "Assembly Level programming language",
+            "Machine level programming language",
+            "None of the above"
         ]
     },
     {
@@ -35,18 +35,20 @@ let questions = [
     },
     {
         numb: 4,
-        question: "What should appear at the very end of your JavaScript?",
-        answer: [
-            "The </script>",
-            "The <script>",
-            "The END statement",
+        question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        answer: "The User's machine running a Web browser",
+        options: [
+            "The Web server",
+            "The User's machine running a Web browser",
+            "A central machine deep within Netscape's corporate offices",
             "None of the above",
         ]
     },
     {
         numb: 5,
         question: "Which of the following are capabilities of functions in JavaScript?",
-        answer: [
+        answer: "Accept parameters",
+        options: [
             "Return a value",
             "Accept parameters and Return a value",
             "Accept parameters",
